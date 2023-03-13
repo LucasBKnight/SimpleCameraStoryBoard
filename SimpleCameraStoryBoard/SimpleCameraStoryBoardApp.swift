808ee7d53e2ed4aff10b6624ee52d998ebf08030
LucasBKnight <lucasbknight@gmail.com>
2023-03-13 08:38:01 -0700

    Initial Commit

---
+++ SimpleCameraStoryBoard/SimpleCameraStoryBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCameraStoryBoardApp.swift
+//  SimpleCameraStoryBoard
+//
+//  Created by Lucas Knight on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCameraStoryBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
